a22ec9f4ec75486078937e2bb56868673eefd387
yahia <yahia.elfellah@molinerocapital.com>
2023-02-15 09:58:32 -0500

    [add] Member page Components

---
+++ frontend/src/components/Member/MemberStatus.vue
@@ -0,0 +1,256 @@
+<template>
+  <el-row :gutter="24">
+    <el-col :span="8">
+      <el-card class="box-card">
+        <template #header>
+          <div class="card-header">
+            <!-- <el-button
+              color="#7325ef"
+              plain
+              round
+              ><el-icon><Plus /></el-icon> Add
+            </el-button> -->
+            <span class="card-title"> Default Messages</span>
+          </div>
+        </template>
+        <div
+          id="default-messages"
+          style="display: flex; flex-direction: column; align-items: stretch"
+        >
+          <div class="input-default">
+            <div class="default-message">
+              <p>Available default:</p>
+              <input
+                type="text"
+                v-model="message.available"
+                maxlength="25"
+                style="background-color: #2ecc7166"
+              />
+            </div>
+          </div>
+
+          <div class="input-default">
+            <div class="default-message">
+              <p>Busy default:</p>
+              <input
+                type="text"
+                v-model="message.busy"
+                maxlength="25"
+                style="background-color: #f1c40f66"
+              />
+            </div>
+          </div>
+
+          <div class="input-default">
+            <div class="default-message">
+              <p>Unavailable default:</p>
+              <input
+                type="text"
+                v-model="message.unavailable"
+                maxlength="25"
+                style="background-color: #e74c3c66"
+              />
+            </div>
+          </div>
+        </div>
+      </el-card>
+    </el-col>
+    <el-col :span="16">
+      <el-card class="box-card">
+        <template #header>
+          <div class="card-header">
+            <!-- <el-button color="#7325ef" plain round
+              ><el-icon><Plus /></el-icon> Add
+            </el-button> -->
+            <span class="card-title">
+              Click on Add to create a custom message in order to be displayed
+              for the users</span
+            >
+            <!-- <DeviceForm
+              :visible="dialogFormVisible"
+              @close="dialogFormVisible = false"
+              @refresh="getData()"
+            /> -->
+          </div>
+        </template>
+        <el-scrollbar height="50vh">
+          <el-skeleton :loading="false" animated :count="3" :throttle="500">
+            <template #template>
+              <div id="skeleton-item" style="width: 100%; height: 200px">
+                <div style="padding: 14px">
+                  <el-skeleton-item variant="h3" style="width: 50%" />
+                  <div>
+                    <el-skeleton-item
+                      variant="text"
+                      style="margin-right: 16px"
+                    />
+                    <el-skeleton-item variant="text" style="width: 30%" />
+                  </div>
+                </div>
+              </div>
+            </template>
+            <template #default>
+              <div style="margin: 1%" v-for="item in 3" :key="item">
+                <StatusElement />
+              </div>
+            </template>
+          </el-skeleton>
+        </el-scrollbar>
+      </el-card>
+    </el-col>
+  </el-row>
+  <!-- 
+    <h2>Your states :</h2>
+
+    <button class="pntr" id="add-state" style="margin-top: 0px">+ State</button>
+    <div id="default-messages" class="default-messages">
+      <h1 style="margin: 100px 0 30px 20px">Default messages :</h1>
+      <div class="input-default">
+        <div id="default-message-available" class="default-message">
+          <p>Available default message :</p>
+          <input type="text" value="{{default.available}}" maxlength="25" />
+        </div>
+        <p class="status green">{{ msg }}</p>
+      </div>
+
+      <div class="input-default">
+        <div id="default-message-busy" class="default-message">
+          <p>Busy default message :</p>
+          <input type="text" value="{{default.busy}}" maxlength="25" />
+        </div>
+        <p class="status orange">{{ msg }}</p>
+      </div>
+
+      <div class="input-default">
+        <div id="default-message-unavailable" class="default-message">
+          <p>Unavailable default message :</p>
+          <input type="text" value="{{default.unavailable}}" maxlength="25" />
+        </div>
+        <p class="status red">{{ msg }}</p>
+      </div>
+    </div>
+
+    <div class="states-control">
+      <button class="pntr" id="save-states">Save</button>
+      <button class="pntr" id="tracker" value="{{tracking}}">
+        Tracker {{ tracking }}
+      </button>
+      <button class="pntr" id="my-flow">My flow</button>
+    </div> -->
+</template>
+<script>
+import StatusElement from "./MemberStatusElement.vue";
+export default {
+  props: {
+    states: {
+      type: Array,
+    },
+  },
+  components: {
+    StatusElement,
+  },
+  data() {
+    return {
+      message: {
+        available: "",
+        busy: "",
+        unavailable: "",
+      },
+    };
+  },
+};
+</script>
+<style>
+.skeleton-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+}
+
+.default-message {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  box-shadow: var(--shadow);
+  background-color: var(--bg);
+  padding: var(--el-card-padding);
+  border-radius: 100px;
+  margin-bottom: 20px;
+  border: 1px solid transparent;
+  float: left;
+  /* width: 450px; */
+}
+
+.default-message > * {
+  padding: 0;
+}
+
+.default-message > p {
+  margin-right: 10px;
+  float: right;
+  font-size: smaller;
+  /* width: 200px; */
+}
+
+.default-message > input {
+  font-size: var(--small-text);
+  padding: 1%;
+  border-radius: 100px;
+  width: 100%;
+}
+
+.default-message:hover {
+  border: 1px solid var(--main);
+}
+
+.input-default {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+
+.input-default > p {
+  font-size: var(--small-text);
+  margin-left: auto;
+}
+
+/* .tracker-on {
+  border-color: var(--green);
+  color: var(--green);
+}
+
+.tracker-on:hover {
+  border-color: var(--red);
+  color: var(--bg);
+  background-color: var(--red);
+}
+
+.tracker-off {
+  border-color: var(--red);
+  color: var(--red);
+}
+
+.tracker-off:hover {
+  border-color: var(--green);
+  color: var(--bg);
+  background-color: var(--green);
+}
+
+#my-flow {
+  width: 150px;
+}
+
+.delete-btn {
+  margin-right: 20px;
+  color: var(--grey);
+}
+
+.delete-btn:hover {
+  cursor: pointer;
+  color: var(--main);
+} */
+</style>
---
+++ frontend/src/components/Member/MemberStatusElement.vue
@@ -0,0 +1,130 @@
+<template>
+  <el-card class="box-card" shadow="hover">
+    <el-form
+      ref="ruleFormRef"
+      :model="ruleForm"
+      label-width="120px"
+      label-position="left"
+    >
+      <el-form-item label="Message">
+        <el-col :span="16">
+          <el-form-item prop="message">
+            <el-input
+              v-model="ruleForm.message"
+              placeholder="Please add a message"
+            />
+          </el-form-item>
+        </el-col>
+        <el-col class="text-center" :span="2">
+          <span class="text-gray-500">-</span>
+        </el-col>
+        <el-col :span="6">
+          <el-form-item prop="color">
+            <el-select v-model="ruleForm.color" placeholder="Color">
+              <el-option
+                v-for="item in cities"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+                <div
+                  style="
+                    display: flex;
+                    flex-direction: row;
+                    align-items: center;
+                    justify-content: space-between;
+                  "
+                >
+                  <span style="float: left">{{ item.label }}</span>
+                  <span
+                    class="square"
+                    :style="{
+                      float: 'right',
+                      'background-color': item.value,
+                      'font-size': '13px',
+                    }"
+                  ></span>
+                </div>
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="Visibility:" prop="visibility">
+        <el-checkbox-group v-model="ruleForm.visibility">
+          <el-checkbox label="Student" name="type" />
+          <el-checkbox label="Researcher" name="type" />
+          <el-checkbox label="Colleague" name="type" />
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item>
+        <!-- <p id="preview-{{id}}" class="status {{color}}">{{ ruleForm.message }}</p> -->
+        <p :style="{ 'background-color': ruleForm.color }">
+          {{ ruleForm.message }}
+        </p>
+        <el-button
+          type="danger"
+          :icon="Delete"
+          plain
+          circle
+          style="margin-left: auto"
+        />
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+<script>
+import { Delete } from "@element-plus/icons-vue";
+export default {
+  // components : {
+  //   Delete,
+  // },
+  prop: {
+    id: {
+      type: String,
+    },
+    ruleForm: {
+      type: Object,
+      default: {
+        color: "",
+        visibility: [],
+        message: "",
+      },
+    },
+  },
+  data() {
+    return {
+      type: "",
+      Delete,
+      ruleForm: {},
+      cities: [
+        {
+          label: "Green",
+          value: "#2ecc7166",
+        },
+        {
+          label: "Yellow",
+          value: "#f1c40f66",
+        },
+        {
+          label: "Red",
+          value: "#e74c3c66",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.box-card > .el-card__body {
+  padding-bottom: 0px !important;
+}
+.el-form-item {
+  margin-bottom: 9px;
+}
+.square {
+  height: 12px;
+  width: 12px;
+  border-radius: 30%;
+}
+</style>
