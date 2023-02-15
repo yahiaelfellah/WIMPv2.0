325c3e92aa28d1f03ff82870d88811e2ef05bf60
yahia <yahia.elfellah@molinerocapital.com>
2023-02-15 09:57:59 -0500

    [add] Member vue page

---
+++ frontend/src/views/Member.vue
@@ -0,0 +1,29 @@
+<template>
+     <div id="profile">
+    <div class="common-layout">
+      <el-header>
+        <Header></Header>
+      </el-header>
+      <el-main>
+        <MemberStatus/>
+      </el-main>
+      <el-footer>
+        <h3>&copy; 2022 wimp</h3>
+      </el-footer>
+    </div>
+  </div> 
+</template>
+<script>
+import Header from '../components/Common/HeaderComponent.vue';
+import MemberStatus from '@/components/Member/MemberStatus.vue';
+export default {
+    name:"vue-member",
+    components:{
+        Header,
+        MemberStatus,
+    }
+}
+</script>
+<style>
+
+</style>
