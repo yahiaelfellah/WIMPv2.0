1d3217a2e0970d84eda455d57daacb9e25967201
yahia <yahia.elfellah@molinerocapital.com>
2023-02-23 18:22:02 -0500

    [update] flow management module

--- flows/routes/controllers/flows.management.js
+++ flows/routes/controllers/flows.management.js
@@ -1,14 +1,16 @@
 const { spawn } = require("child_process");
 const NodeCache = require("node-cache");
 const portfinder = require("portfinder");
+const { sender } = require("../../messaging/flows.messaging");
 
 const cache = new NodeCache(); // create a new cache instance
 /**
  * Start A Node Red Instance
- * TODO: Check the OS we are running on in order to have 
+ * TODO: Check the OS we are running on in order to have
  * TODO: Send The information to user in order to store the information of the process
  */
-exports.startNodeRed = async () => {
+exports.startNodeRed = async (id) => {
+  console.info("--------- Getting Instruction to run a Node Red Instance ------------------------")
   const port = await portfinder.getPortPromise();
   const nodeRedProcess = spawn("node-red.cmd", ["-v", "-p", port], {
     detached: true,
@@ -16,15 +18,22 @@ exports.startNodeRed = async () => {
   });
   nodeRedProcess.unref();
   nodeRedProcess.on("error", (err) => {
-    console.error("Failed to spawn Node-RED process:", err);
+    console.error("Failed to spawn Node-RED process:", err);  
   });
   // Check if the Node-RED process is running
   if (nodeRedProcess.pid !== null) {
-    console.log("Node-RED process is running with PID:", nodeRedProcess.pid);
+    console.info("Node-RED process is running with PID:", nodeRedProcess.pid);
+    cache.set(`node-red-${nodeRedProcess.pid}`, nodeRedProcess);
+    // sender({
+    //   action: "patch_nodered_instance",
+    //   data: {
+    //     id: id,
+    //     body: nodeRedProcess,
+    //   },
+    // });
   } else {
     console.log("Node-RED process is not running");
   }
-  cache.set(`node-red-${nodeRedProcess.pid}`, nodeRedProcess);
 };
 /**
  * Kill the Node Process
@@ -35,7 +44,7 @@ exports.CloseNodeRed = (pid) => {
   nodeRedProcess.kill("SIGTERM");
   // Listen for the process to exit
   nodeRedProcess.on("exit", (code, signal) => {
-    console.log(
+    console.info(
       `Node-RED process exited with code ${code} and signal ${signal}`
     );
   });
